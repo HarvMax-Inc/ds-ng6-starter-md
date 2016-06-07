dbf38b01085d885d9d573313256dc1a8602cbbe5
Zack Vera <zack.vera@stgconsulting.com>
2016-06-07 17:58:41 -0600

    Removed unneeded common boilerplate components

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,16 +1,10 @@
 import angular from 'angular';
-import Navbar from './navbar/navbar';
-import Hero from './hero/hero';
-import User from './user/user';
 import dsMenuItem from './dsMenuItem/dsMenuItem';
 import dsMenu from './dsMenu/dsMenu';
 import dsNavItem from './dsNavItem/dsNavItem';
 import dsNavbar from './dsNavbar/dsNavbar';
 
 let commonModule = angular.module('app.common', [
-  Navbar.name,
-  Hero.name,
-  User.name,
   dsNavbar.name,
   dsMenuItem.name,
   dsMenu.name,
